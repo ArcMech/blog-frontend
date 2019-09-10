6f91f6b6ad0824e7a3f44f0bbb6e82993a495e29
ArcMech <arek.krz@gmail.com>
2019-09-10 20:20:59 +0200

    changed contact form for just contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,65 +1,22 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
 import styles from "../styles/contact.module.css";
-import axios from "axios";
 
 const Contact = () => {
   return (
     <section className={styles.contact_page}>
-      <h1>Napisz, jeśli chcesz abym pomógł</h1>
-      <p>
-        Funkcja jeszcze nie działa :(, skontaktuj się poprzez media
-        społecznościowe.
-      </p>
-      <Formik
-        validate
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          content: ""
-        }}
-        onSubmit={values => {
-          setTimeout(() => {
-            axios
-              .post("http://127.0.0.1:8000/api/questions/", values)
-              .then(res => {
-                alert("Wysłane", res.data);
-              })
-              .catch(err => {
-                alert(err);
-              });
-            // console.log(JSON.stringify(values, null, 2));
-          }, 500);
-        }}
-        render={() => (
-          <Form>
-            <label htmlFor="firstName">Imię</label>
-            <Field name="firstName" placeholder="Imię" required />
-
-            <label htmlFor="lastName">Nazwisko</label>
-            <Field name="lastName" placeholder="Nazwisko" required />
-
-            <label htmlFor="email">Email</label>
-            <Field
-              name="email"
-              placeholder="email@acme.com"
-              type="email"
-              required
-            />
-
-            <label htmlFor="content">Twoja wiadomość</label>
-            <Field
-              component="textarea"
-              name="content"
-              placeholder="Czego potrzebujesz?"
-              type="content"
-              required
-            />
-            <button type="submit">Wyślij gołębia! Grrru!</button>
-          </Form>
-        )}
-      />
+      <h1>W czym mogę pomóc?</h1>
+      <div className={styles.contact_data}>
+        <h4 className={styles.contact_info}>
+          Jeżeli chcesz ze mną pracować, z chęcią podejmę współpracę! <br></br>
+          Napisz do mnie maila, a potem przejdziemy do rozmowy w wygodny dla nas
+          obu sposób.
+        </h4>
+        <h4 className={styles.contact_info}>
+          Napisz do mnie na
+          <a href="mailto:arek.krz@gmail.com"> arek.krz@gmail.com</a>
+        </h4>
+        <h4 className={styles.contact_info}>Wielkie dzięki! Do zobaczenia!</h4>
+      </div>
     </section>
   );
 };
